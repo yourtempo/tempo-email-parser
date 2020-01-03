d2958e0fd3275f4a8be3d6df7c846a341f1ac50f
Soreine <hello@soreine.dev>
2020-01-03 17:23:30 +0100

    Move email docs to code

--- benchmarks/emails/index.ts
+++ benchmarks/emails/index.ts
@@ -6,10 +6,18 @@ function readFile(relativePath: string): string {
 }
 
 const EMAILS = {
-	BASIC: readFile('basic-lorem-gmail.html'),
-	BASIC_REPLIED_X1: readFile('basic-lorem-gmail-replied-x1.html'),
-	BASIC_REPLIED_X2: readFile('basic-lorem-gmail-replied-x2.html'),
-	MARKETING: readFile('marketing-gmail.html'),
+	/*
+	BASICS are basic emails with inline formatting such as bold. `x1` and `x2` are self-replied versions of the first. They increase in size approximately by a linear factor of 1, 2 and 3.
+	*/
+	BASIC: readFile('basic-lorem-gmail.html'), // 1.7K
+	BASIC_REPLIED_X1: readFile('basic-lorem-gmail-replied-x1.html'), // 3.7K
+	BASIC_REPLIED_X2: readFile('basic-lorem-gmail-replied-x2.html'), // 6.2K
+
+	/*
+	This is a real-world marketing email. It has a large size, and contains heavy HTML and CSS.
+	*/
+
+	MARKETING: readFile('marketing-gmail.html'), // 102K
 };
 
 export default EMAILS;
