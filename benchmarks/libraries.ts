6b5234eebf3980a4965c2288b6ef59a611386010
Soreine <hello@soreine.dev>
2020-01-08 15:16:30 +0100

    Cleanup code

--- benchmarks/libraries.ts
+++ benchmarks/libraries.ts
@@ -3,7 +3,7 @@ These benchmarks compare different libraries (not all are used by this project).
 This helps estimate the cost of using any of them.
  */
 
-import jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 import cheerio from 'cheerio';
 import Benchmark from 'benchmark';
 import Talon from 'talonjs';
@@ -24,13 +24,13 @@ suite.on('cycle', (event: any) => {
 // Test a simple parsing on basic HTML. Using a linear scale of input complexity, we can see if the performance is linear or worst.
 suite
 	.add('Parse # JSDom # Size 1', () => {
-		new jsdom.JSDOM(EMAILS.BASIC);
+		new JSDOM(EMAILS.BASIC);
 	})
 	.add('Parse # JSDom # Size 2', () => {
-		new jsdom.JSDOM(EMAILS.BASIC_REPLIED_X1);
+		new JSDOM(EMAILS.BASIC_REPLIED_X1);
 	})
 	.add('Parse # JSDom # Size 3', () => {
-		new jsdom.JSDOM(EMAILS.BASIC_REPLIED_X2);
+		new JSDOM(EMAILS.BASIC_REPLIED_X2);
 	})
 	.add('Parse # Cheerio # Size 1', () => {
 		cheerio.load(EMAILS.BASIC);
@@ -45,7 +45,7 @@ suite
 // Test parsing a real-world, HTML-heavy, marketing email
 suite
 	.add('Parse # JSDom # Marketing email', () => {
-		new jsdom.JSDOM(EMAILS.MARKETING);
+		new JSDOM(EMAILS.MARKETING);
 	})
 	.add('Parse # Cheerio # Marketing email', () => {
 		cheerio.load(EMAILS.MARKETING);
@@ -60,7 +60,7 @@ suite
 		Talon.quotations.extractFromHtml(EMAILS.MARKETING);
 	})
 	.add('Quotation # Planer with JSDom # Marketing email', () => {
-		const document = new jsdom.JSDOM().window.document;
+		const document = new JSDOM().window.document;
 		planer.extractFrom(EMAILS.MARKETING, 'text/html', document);
 	});
 
