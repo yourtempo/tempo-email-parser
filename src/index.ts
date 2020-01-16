c76941e99fe24ac0a7d8a3f1bedb1b09172048de
Soreine <hello@soreine.dev>
2020-01-16 17:54:28 +0100

    Only export prepareMessage and linkify

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import linkify from './linkify';
-import removeQuotations from './removeQuotations';
-import removeTrailingWhitespaces from './removeTrailingWhitespaces';
 import prepareMessage from './prepareMessage';
 
 export default prepareMessage;
-// For development purpose, also expose other main functions for now
-export { removeQuotations, linkify, removeTrailingWhitespaces };
+export { linkify };
