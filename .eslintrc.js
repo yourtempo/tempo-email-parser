4579834f6405153fff5175c8fec4a7994b154057
Soreine <hello@soreine.dev>
2020-03-24 13:50:07 +0100

    Fix eslint config to work with Typescript
    Following this great resource
    https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,21 +6,22 @@ module.exports = {
 		node: true,
 	},
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:prettier/recommended',
+		'plugin:@typescript-eslint/recommended', // Base rules configuration from Eslint, adapted to Typescript
+		'prettier/@typescript-eslint', // disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors,, so that they can be fixed automatically with `eslint --fix`. Make sure this is always the last configuration in the extends array.
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'module',
 	},
-	plugins: ['prettier', '@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'prettier'],
 	rules: {
 		indent: ['off', 'tab', { SwitchCase: 1 }],
 		'linebreak-style': ['error', 'unix'],
 		'no-useless-escape': [0, 'unix'],
 		semi: ['error', 'always'],
+		'@typescript-eslint/no-use-before-define': 'off',
 	},
 	ignorePatterns: ['./old/', './node_modules/'],
 };
